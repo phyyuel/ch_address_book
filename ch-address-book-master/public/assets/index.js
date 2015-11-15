8a25c7f734ea018ba0f81938b1a81dc513f88ada
phyyuel <phy.yuel@gmail.com>
2015-11-15 01:52:03 -0800

    Edit and Detail
    Edit and Detail

--- ch-address-book-master/public/assets/index.js
+++ ch-address-book-master/public/assets/index.js
@@ -1,83 +1,39 @@
 angular.module('app', ['ngRoute', 'angular-toArrayFilter'])
 .factory('Contacts',function($q, $http){
-//	var val = null;
-//
-//	function LoadData() {
-//		var defer = $q.defer();
-//		$http.get('assets/contacts.json').success(function (data) {
-//			val = data;
-//				
-//			defer.resolve();
-//		});
-//		return defer.promise;
 		return $http.get('assets/contacts.json');
-//	}
-
-//	return {
-//		GetData: function () { return val ; },
-//		LoadData:LoadData
-//	}
 })
 
 .controller('ContactsController', ['$scope', 'Contacts', function ($scope, Contacts) {
-//		$scope.contacts = Contacts.GetData();
-		$scope.contacts = Contacts.success(function(response){
-			$scope.contacts = response.contacts;
-		});
-
+	$scope.contacts = Contacts.success(function(response){
+		$scope.contacts = response.contacts;
+		$scope.selected = $scope.contacts[0]
+		$scope.selectedId = 0;
+		$scope.editorEnabled = false;
+	});
+	
+	$scope.getDetail = function(id, event){
+		$scope.selected = $scope.contacts[id];
+		console.log($scope.selected);
+		$scope.selectedId = id;
+	};
+	
 }])
 
-//.controller('TodoController', ['$scope', 'Todos', function ($scope, Todos) {
-	//	$scope.todos = Todos;
-	//}])
+.controller('ContactsEditController', ['$scope', '$routeParams', 'Contacts', function ($scope, $routeParams, Contacts) {
+	$scope.contact = Contacts.success(function(response){
+		$scope.contact = response.contacts[$routeParams.id];
+	});
 	
+}])
+		
 .config(['$routeProvider', function ($routeProvider) {
 	$routeProvider
 		.when('/', {
 			templateUrl: '/contacts.html',
 			controller: 'ContactsController',
-//			resolve:{
-//					load:function(Contacts){
-//							return Contacts.LoadData();
-//							}
-//			}
+		})
+		.when('/:id', {
+			templateUrl: '/contactEdit.html',
+			controller: 'ContactsEditController',
 		});
 }]);
-
-//
-//angular.module('app', ['ngRoute'])
-//
-//.factory(' ', function(){
-//	return [
-//		{ name: 'AngularJS Directives', completed: true, note: 'add notes...' },
-//		{ name: 'Data binding', completed: true, note: 'add notes...' },
-//		{ name: '$scope', completed: true, note: 'add notes...' },
-//		{ name: 'Controllers and Modules', completed: true, note: 'add notes...' },
-//		{ name: 'Templates and routes', completed: true, note: 'add notes...' },
-//		{ name: 'Filters and Services', completed: false, note: 'add notes...' },
-//		{ name: 'Get started with Node/ExpressJS', completed: false, note: 'add notes...' },
-//		{ name: 'Setup MongoDB database', completed: false, note: 'add notes...' },
-//		{ name: 'Be awesome!', completed: false, note: 'add notes...' },
-//	];
-//})
-//
-//.controller('TodoController', ['$scope', 'Todos', function ($scope, Todos) {
-//	$scope.todos = Todos;
-//}])
-//	
-//.controller('TodoDetailCtrl', ['$scope', '$routeParams', 'Todos', function ($scope, $routeParams, Todos) {
-//	$scope.todo = Todos[$routeParams.id];
-//}])
-//	
-//.config(['$routeProvider', function ($routeProvider) {
-//	$routeProvider
-//		.when('/', {
-//			templateUrl: '/todos.html',
-//			controller: 'TodoController'
-//		})
-//	
-//		.when('/:id', {
-//			templateUrl: '/todoDetails.html',
-//			controller: 'TodoDetailCtrl'
-//	 });
-//}]);
