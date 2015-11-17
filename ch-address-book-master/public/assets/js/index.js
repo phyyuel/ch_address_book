9bb2267e84d630dbb2f0efb17762734cf44f3a4c
phyyuel <phy.yuel@gmail.com>
2015-11-16 18:36:58 -0800

    Working
    Working

--- ch-address-book-master/public/assets/js/index.js
+++ ch-address-book-master/public/assets/js/index.js
@@ -4,29 +4,29 @@ angular.module('app', ['ngRoute', 'angular-toArrayFilter'])
 })
 
 .controller('ContactsController', ['$scope', 'Contacts', function ($scope, Contacts) {
-	$scope.contacts = Contacts.success(function(response){
-		$scope.contacts = response.contacts;
-		$scope.selected = $scope.contacts[0]
-		$scope.selectedId = 0;
-		$scope.editorEnabled = false;
-	});
 	
-	$scope.getDetail = function(id, event){
-		$scope.selected = $scope.contacts[id];
-		console.log($scope.selected);
-		$scope.selectedId = id;
-	};
+	Contacts.then(function(response){
+			$scope.contacts = response.data.contacts;
+			$scope.selected = $scope.contacts[0];
+			$scope.selectedId = 0;
+			$scope.editorEnabled = false;
+			
+			$scope.getDetail = function(id, event){
+				$scope.selected = $scope.contacts[id];
+				$scope.selectedId = id;
+			};
+			
+			$scope.enableEdit = function(){
+				$scope.editorEnabled = true;
+			};
+			
+			$scope.disableEdit = function(){
+				$scope.editorEnabled = false;
+				
+			};	
+		});
 	
-	$scope.enableEdit = function(){
-		$scope.editorEnabled = true;
-		console.log("edit clilcked, enable edit: " + $scope.editorEnabled);
-	};
 	
-	$scope.disableEdit = function(){
-		$scope.editorEnabled = false;
-		console.log("edit finished, enable eidt: " + $scope.editorEnabled);
-		
-	};		
 }])
 
 .controller('ContactsEditController', ['$scope', '$routeParams', 'Contacts', function ($scope, $routeParams, Contacts) {
